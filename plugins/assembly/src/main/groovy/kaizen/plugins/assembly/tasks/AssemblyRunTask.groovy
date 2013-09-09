4273569b3a245fd4ac6856d09b3ca12168b65b41
Rodrigo B. de Oliveira <rbo@acm.org>
2013-09-09 11:03:01 -0300

    fix AssemblyRunTask to use the new assembly/clr model

--- plugins/assembly/src/main/groovy/kaizen/plugins/assembly/tasks/AssemblyRunTask.groovy
+++ plugins/assembly/src/main/groovy/kaizen/plugins/assembly/tasks/AssemblyRunTask.groovy
@@ -1,32 +1,35 @@
 package kaizen.plugins.assembly.tasks
 
+import kaizen.plugins.assembly.model.Assembly
+import kaizen.plugins.clr.Clr
+import kaizen.plugins.clr.ClrExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class AssemblyRunTask extends DefaultTask {
 
-	def monoExecutable
-
-	def workingDir
-
 	def assembly
 
 	def runAssemblyBuiltBy(AssemblyCompile compileTask) {
-		this.monoExecutable = { project.rootProject.extensions.unity.mono.cli }
-		this.workingDir = { compileTask.resolvedOutputDir }
-		this.assembly = { compileTask.assemblyFile }
+		this.assembly = { compileTask.outputAssembly }
 		this.dependsOn compileTask
-		this.onlyIf { compileTask.assembly.isExecutable() }
+		this.onlyIf { assemblyModel.isExecutable() }
 	}
 
 	@TaskAction
 	def run() {
 		def task = this
-		project.exec {
-			workingDir project.file(task.workingDir)
-			commandLine project.file(task.monoExecutable)
-			args "--debug"
-			args project.file(task.assembly)
+		clr.exec {
+			executable project.file(task.assembly)
 		}
+	}
+
+	Clr getClr() {
+		def clr = ClrExtension.forProject(project)
+		clr.runtimeForFrameworkVersion(assemblyModel.targetFrameworkVersion)
+	}
+
+	Assembly getAssemblyModel() {
+		Assembly.forProject(project)
 	}
 }
