f71b7054d472d8ad94b9e64ecbbc99a0010177e7
a.navrotskiy <navrotskiy@corp.mail.ru>
2014-04-28 10:48:16 +0400

    Fix nunit-assemble.

--- plugins/nunit/src/main/groovy/kaizen/plugins/nunit/NUnitAssemblyPlugin.groovy
+++ plugins/nunit/src/main/groovy/kaizen/plugins/nunit/NUnitAssemblyPlugin.groovy
@@ -1,13 +1,13 @@
 package kaizen.plugins.nunit
 
 import kaizen.plugins.assembly.AssemblyPlugin
+import kaizen.plugins.assembly.tasks.AssemblyCompile
+import kaizen.plugins.conventions.Configurations
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
 import org.gradle.util.ConfigureUtil
-import kaizen.plugins.conventions.Configurations
-import kaizen.plugins.assembly.tasks.AssemblyCompile
-import org.gradle.api.Task
 
 class NUnitAssemblyPlugin implements Plugin<Project> {
 
@@ -23,7 +23,7 @@ class NUnitAssemblyPlugin implements Plugin<Project> {
 		def masterTestTask = project.task('test') {
 			description 'Runs all nunit tests.'
 		}
-		bundle.afterEvaluate {
+		project.afterEvaluate {
 			configureNUnitTasksOn project, masterTestTask
 		}
 	}
@@ -42,15 +42,13 @@ class NUnitAssemblyPlugin implements Plugin<Project> {
 			if (compileTask) {
 				project.dependencies.add(config.name, "nunit:nunit.framework:${nunitVersion}")
 				configure(project) {
-					afterEvaluate {
-						def outputAssembly = project.file(compileTask.outputAssembly)
-						def testConfigTask = task("test$configLabel", type: NUnitTask, dependsOn: [compileTask, rootProject.tasks.updateNUnit]) {
-							inputs.file outputAssembly
-							outputs.file new File(outputAssembly.parentFile, 'TestResult.xml')
-							targetFrameworkVersion compileTask.targetFrameworkVersion
-						}
-						masterTestTask.dependsOn(testConfigTask)
+					def outputAssembly = project.file(compileTask.outputAssembly)
+					def testConfigTask = task("test$configLabel", type: NUnitTask, dependsOn: [compileTask, rootProject.tasks.updateNUnit]) {
+						inputs.file outputAssembly
+						outputs.file new File(outputAssembly.parentFile, 'TestResult.xml')
+						targetFrameworkVersion compileTask.targetFrameworkVersion
 					}
+					masterTestTask.dependsOn(testConfigTask)
 				}
 			}
 		}
